b6b8b157fd9444b44c495ef77d056cf2be3dbb35
Averiser <averiser@gmail.com>
2021-11-25 18:48:39 +0300

    first commit

---
+++ Calculator Layout iOS13/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Calculator Layout iOS13
+//
+//  Created by Angela Yu on 01/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var displayLabel: UILabel!
+  
+  
+  @IBAction func calcButtonPressed(_ sender: UIButton) {
+    // What happens when a non-number button is pressed
+  }
+  
+  @IBAction func numButtonPressed(_ sender: UIButton) {
+  }
+  
+  
+  override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
